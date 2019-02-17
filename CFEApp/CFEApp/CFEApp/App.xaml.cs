3234764c7e68e1cc72775350d43a9117f7f6b1cd
Luis David De La Cruz Bautista <delacruz_luis@outlook.es>
2019-02-17 00:11:30 -0600

    sln files

---
+++ CFEApp/CFEApp/CFEApp/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Plugin.Connectivity;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace CFEApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                MainPage = new NavigationPage(new OffLineApp());
+            }
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                MainPage = new NavigationPage(new OffLineApp());
+            }
+        }
+    }
+}
---
+++ CFEApp/CFEApp/CFEApp/OffLineApp.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CFEApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class OffLineApp : ContentPage
+	{
+		public OffLineApp ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
